091970a8346aaa9c66b84770e09d64077ad3e93c
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-06-22 14:20:14 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,52 @@
 import React, { useEffect, useState}  from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, Text, FlatList, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 import api from './services/api';
 
 export default function App () {
     const [projects, setProjects] = useState([]);
 
     useEffect(() => {
-        api.get('projects').then(response => {
-            console.log(response.data);
-            
+        api.get('projects').then(response => {         
             setProjects(response.data);            
         });
     }, []);
 
+    async function handleAddProject(){
+        const response = await api.post('projects',{
+            title: `Novo projeto ${Date.now()}`,
+            owner: 'Robinson Lovatto'
+        });
+
+        const project = response.data;
+
+        setProjects([...projects,project]);
+    }
+
     return (
         <>
             <StatusBar barStyle='light-content' backgroundColor='#7179c1'></StatusBar>
-            <View style={styles.container}>
-                <Text style={styles.title} >Hello GoStack</Text>
-            </View>
+            
+            <SafeAreaView style={styles.container}>
+                <FlatList 
+                    data={projects} //deve ser um array
+                    keyExtractor={project => project.id}
+                    renderItem={({item:project}) => ( //desestruturação do argumento item dentro da variável que é renomeada para project
+                        <Text style={styles.project}> {project.title} </Text>
+                    )}
+                
+                /> 
+            </SafeAreaView> 
+
+            <TouchableOpacity activeOpacity={0.6} style={styles.button} onPress={handleAddProject}>
+                <Text style={styles.buttonText}>Adicionar projeto</Text>
+            </TouchableOpacity>
+
+
+            {/* <View style={styles.container}>
+                {projects.map(project => (
+                    <Text style={styles.project} key={project.id} > {project.title} </Text>
+                ))}
+            </View> */}
         </>
     );
     
@@ -28,12 +56,21 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#7159c1',
-        justifyContent: 'center',
-        alignItems: 'center'
     },
-    title: {
+    project: {
         color: '#FFF',
-        fontSize: 32,
-        fontWeight: 'bold'
-    }
+        fontSize: 30
+    },
+    button: {
+        backgroundColor: '#FFF',
+        margin: 20,
+        height: 50,
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
 })
